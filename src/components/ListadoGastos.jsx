54e596703e46e29e992224abe5ba05e647910a73
Luis De La Torre <darkknightnadroid@gmail.com>
2023-12-07 20:50:34 -0600

    Correccion de Listado de Gastos

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -4,8 +4,6 @@ import Gasto from "./Gasto"
 function ListadoGastos({ gastos, setGastoEditar, eliminarGasto, filtro, gastosFiltrados }) {
   return (
     <div className="listado-gastos contenedor">
-      <h2>{gastos.length ? 'Gastos' : 'No hay Gastos aún'}</h2>
-
       {
         filtro ? (
           <>
