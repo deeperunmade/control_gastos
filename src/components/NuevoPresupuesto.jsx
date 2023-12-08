8d9757218993b3ae412b3cf48dd1dbcc70b90cda
Luis De La Torre <darkknightnadroid@gmail.com>
2023-12-07 20:41:33 -0600

    App base terminada

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Mensaje from "./Mensaje";
+
+
+function NuevoPresupuesto({presupuesto,setPresupuesto, setIsValidPresupuesto}) {
+
+    const [mensaje, setMensaje] = useState('');
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+        
+        if(presupuesto <= 0){
+            setMensaje('No es un presupuesto válido')
+            return
+        }
+        setMensaje('');
+        setIsValidPresupuesto(true);
+
+    }
+    
+  return (
+    <div className='contenedor-presupuesto contenedor sombra'>
+        <form onSubmit={handlePresupuesto} className="formulario">
+            <div className='campo'>
+                <label htmlFor="presupuesto">Definir Presupuesto</label>
+                <input id="presupuesto" type="number" value={presupuesto} onChange={(e) => setPresupuesto(Number(e.target.value))} className='nuevo-presupuesto' placeholder='Añade tu Presupuesto' />
+            </div>
+            <input type="submit" value="Añadir" />
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
